baf55249ecb99f44c78aa555d73879a5b12b4240
vladislav <vladislav.kolos.y@gmail.com>
2024-11-26 12:35:00 +0300

    Init commit

---
+++ build.gradle.kts
@@ -0,0 +1,71 @@
+plugins {
+    id("org.springframework.boot") version "3.2.5"
+    id("io.spring.dependency-management") version "1.1.0"
+    kotlin("jvm") version "1.9.23"
+    kotlin("plugin.spring") version "1.9.23"
+    kotlin("plugin.jpa") version "1.9.23"
+    kotlin("kapt") version "1.9.23"
+    id("org.flywaydb.flyway") version "9.22.1"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter") {
+        exclude(group = "org.springframework.boot", module = "spring-boot-starter-logging")
+    }
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-log4j2")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-mail")
+    implementation("org.springframework.boot:spring-boot-starter-aop")
+
+    runtimeOnly("org.apache.tomcat.embed:tomcat-embed-jasper")
+
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+
+//    compileOnly("org.projectlombok:lombok:1.18.32")
+//    kapt("org.projectlombok:lombok:1.18.32")
+
+    runtimeOnly("org.postgresql:postgresql:42.3.1")
+
+    implementation("com.zaxxer:HikariCP:5.0.0")
+
+    implementation("org.flywaydb:flyway-core")
+
+    implementation("org.mapstruct:mapstruct:1.5.5.Final")
+    kapt("org.mapstruct:mapstruct-processor:1.5.5.Final")
+
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.10.2")
+    testImplementation("org.mockito:mockito-core:5.11.0")
+    testImplementation("org.mockito:mockito-junit-jupiter:5.12.0")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "org.example.Application"
+    }
+}
+
+flyway {
+    url = "jdbc:postgresql://localhost:5432/provider_db"
+    user = "postgres"
+    password = "postgresql"
+}
